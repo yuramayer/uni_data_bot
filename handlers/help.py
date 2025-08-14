156130bad7747ee79149af67d76976f39717ed5a
yuramayer <votetopovorot2015@gmail.com>
2025-08-14 22:02:08 +0300

    fix: updated help.py with lint

--- handlers/help.py
+++ handlers/help.py
@@ -1,8 +1,11 @@
+"""Help handler for the bot"""
+
+import time
+import datetime
+from requests import RequestException
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
-import time
-import datetime
 from back.cloud import get_key, send_logs
 from filters.admin_checker import IsAdmin
 from config.conf import admins_ids
@@ -16,18 +19,20 @@ help_router.message.filter(
 
 @help_router.message(Command('help'))
 async def cmd_help(message: Message):
-
+    """Command 'help' for the admins"""
     timestamp = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
     start = time.monotonic()
     user_id = message.chat.id
 
     user_message = message.text
-    assert type(user_message) == str
+    assert isinstance(user_message, str)
 
-    await message.answer('Я - бот с ChatGPT 😉\n\nСпроси меня вопрос, '
-                         'и я постараюсь ответить.\n\n' \
-                         'Разработчик: <b>@botrqst</b>')
-    
+    msg = (
+        'Я - бот с ChatGPT 😉\n\nСпроси меня вопрос, '
+        'и я постараюсь ответить.\n\n'
+        'Разработчик: <b>@botrqst</b>'
+    )
+    await message.answer(msg)
 
     working_ms = int((time.monotonic() - start) * 1000)
 
@@ -39,11 +44,11 @@ async def cmd_help(message: Message):
         "bot_working_ms": working_ms,
         "answer": '[help_message]'
     }
-    
+
     try:
         key = get_key(timestamp, user_id)
 
         send_logs(log_json, key)
 
-    except Exception as err:
+    except (ValueError, TypeError, KeyError, RequestException):
         await message.answer('При записи логов произошла ошибка 😔')
